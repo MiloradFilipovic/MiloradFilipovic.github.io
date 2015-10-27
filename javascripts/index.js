cfe503d1a0acf48055a6c06c6b0f5900511beaee
Milorad Filipovic <miloradfilipovic19@gmail.com>
2015-10-27 15:44:42 +0100

    Setting up mobile events.

--- javascripts/index.js
+++ javascripts/index.js
@@ -125,11 +125,7 @@ $(document).ready(function(e) {
 
     if(touch_ready) {
         $(".hoverLink").bind('touchstart touchend', function(e) {
-            var activates = $(this).find('.continent').attr('data-activates');
-            var target_el = $('.' + activates);
-            if(!target_el.is(':visible')) {
-                e.preventDefault();
-            }
+            e.preventDefault();
         });
 
 
