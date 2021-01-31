f353ff702719e197ac5f1052666a234bc4493092
Milorad Filipovic <milorad.filipovic19@gmail.com>
2021-01-31 23:50:59 +0100

    Finished setting up webpack build
    - Configured build for multiple entries
    - Updated HTML files to use built jS
    - Updated documentation
    - Removed old files

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/javascript/index.js',
+  mode: 'production',
+  entry: {
+    index: './src/javascript/index.js',
+    game: './src/javascript/game-script.js',
+    spinster: './src/javascript/spinster.js'
+  },
   output: {
-    filename: 'index.min.js',
+    filename: '[name].min.js',
     path: path.resolve(__dirname, 'javascript'),
   },
   watch: true,
   watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
     ignored: '**/node_modules'
   }
 };
